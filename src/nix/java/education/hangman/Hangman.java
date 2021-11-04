1cf655cf77afa5fb7cea75657d5593ac37ffd6f7
dsolomatinn <d.v.solomatin@student.khai.edu>
2021-11-04 14:02:02 +0200

    STAGE #1 HANGMAN_GAME

---
+++ src/nix/java/education/hangman/Hangman.java
@@ -0,0 +1,8 @@
+package nix.java.education.hangman;
+
+public class Hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN\nThe game will be available soon...");
+    }
+
+}
