a33a6d29b75d6026157bd734138ecfb221a486ff
dsolomatinn <d.v.solomatin@student.khai.edu>
2021-11-04 14:13:22 +0200

    STAGE #2 HANGMAN_GAME

--- src/nix/java/education/hangman/Hangman.java
+++ src/nix/java/education/hangman/Hangman.java
@@ -1,8 +1,21 @@
 package nix.java.education.hangman;
 
+import java.util.Scanner;
+
 public class Hangman {
     public static void main(String[] args) {
+        //STAGE 1
         System.out.println("HANGMAN\nThe game will be available soon...");
+        //STAGE 2
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Guess the word: ");
+        String answer = scanner.nextLine();
+        if (answer.equals("java")){
+            System.out.println("You survived!!");
+        }
+        else {
+            System.out.println("You lost!");
+        }
     }
 
 }
