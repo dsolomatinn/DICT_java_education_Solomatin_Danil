ccbd614a7b7d4183532e0e18cf5a321884e3dfad
dsolomatinn <d.v.solomatin@student.khai.edu>
2021-11-11 18:51:13 +0200

    STAGE #3 HANGMAN_GAME

--- src/nix/java/education/hangman/Hangman.java
+++ src/nix/java/education/hangman/Hangman.java
@@ -1,21 +1,33 @@
 package nix.java.education.hangman;
-
-import java.util.Scanner;
-
+import java.util.*;
 public class Hangman {
+
     public static void main(String[] args) {
         //STAGE 1
         System.out.println("HANGMAN\nThe game will be available soon...");
         //STAGE 2
         Scanner scanner = new Scanner(System.in);
+//        System.out.print("Guess the word: ");
+//        String answer = scanner.nextLine();
+//        if (answer.equals("java")){
+//            System.out.println("You survived!!");
+//        }
+//        else {
+//            System.out.println("You lost!");
+//        }
+        //STAGE 3
+        String[] words={"python", "java", "kotlin", "javascript"};
+        Random r=new Random();
+        int randomNumber=r.nextInt(words.length);
         System.out.print("Guess the word: ");
         String answer = scanner.nextLine();
-        if (answer.equals("java")){
+        if (answer.equals(words[randomNumber])){
             System.out.println("You survived!!");
         }
         else {
             System.out.println("You lost!");
         }
-    }
 
+
+    }
 }
