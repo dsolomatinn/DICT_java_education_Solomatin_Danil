f314cf219bb1dd04847342a2d7e07ac327587911
dsolomatinn <d.v.solomatin@student.khai.edu>
2021-11-12 08:09:48 +0200

    coffeeMachine stage#2

--- src/nix/java/education/coffemachine/Main.java
+++ src/nix/java/education/coffemachine/Main.java
@@ -1,5 +1,7 @@
 package nix.java.education.coffemachine;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -13,5 +15,19 @@ public class Main {
                 Pouring some milk into the cup
                 Ingridients is ready!
                 """);
+
+
+        //Stage2
+        System.out.println("Write how many cup of coffee you will need:");
+        Scanner scanner = new Scanner(System.in);
+        int cupsOfCoffee = scanner.nextInt();
+        final int water = 200;
+        final int milk = 50;
+        final int coffeeBeams = 15;
+        System.out.println("For" + " " + cupsOfCoffee + " " + "coffee you will need:");
+        System.out.println(water * cupsOfCoffee + " " + "ml of water");
+        System.out.println(milk * cupsOfCoffee + " " + "ml of milk");
+        System.out.println(coffeeBeams * cupsOfCoffee + " " + "g of coffeeBeams");
+
     }
 }
