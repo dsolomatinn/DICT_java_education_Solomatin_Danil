2c674362d59ac14a58507be621a144d36ee37428
dsolomatinn <d.v.solomatin@student.khai.edu>
2021-11-12 08:28:09 +0200

    coffeMachine stage#5,6

--- src/nix/java/education/coffemachine/Main.java
+++ src/nix/java/education/coffemachine/Main.java
@@ -21,62 +21,62 @@ public class Main {
                 """);
 
 
-        //Stage 2
-//        System.out.println("Write how many cup of coffee you will need:");
-//        Scanner scanner = new Scanner(System.in);
-//        int cupsOfCoffee = scanner.nextInt();
-//        final int water = 200;
-//        final int milk = 50;
-//        final int coffeeBeams = 15;
-//        System.out.println("For" + " " + cupsOfCoffee + " " + "coffee you will need:");
-//        System.out.println(water * cupsOfCoffee + " " + "ml of water");
-//        System.out.println(milk * cupsOfCoffee + " " + "ml of milk");
-//        System.out.println(coffeeBeams * cupsOfCoffee + " " + "g of coffeeBeams");
+/*        Stage 2
+        System.out.println("Write how many cup of coffee you will need:");
+        Scanner scanner = new Scanner(System.in);
+        int cupsOfCoffee = scanner.nextInt();
+        final int water = 200;
+        final int milk = 50;
+        final int coffeeBeams = 15;
+        System.out.println("For" + " " + cupsOfCoffee + " " + "coffee you will need:");
+        System.out.println(water * cupsOfCoffee + " " + "ml of water");
+        System.out.println(milk * cupsOfCoffee + " " + "ml of milk");
+        System.out.println(coffeeBeams * cupsOfCoffee + " " + "g of coffeeBeams");
 
-        //Stage 3
+        Stage 3
 
-//        System.out.println("Write how many ml of water you will have:");
-//        int amountOfWater = scanner.nextInt();
-//        System.out.println("Write many how ml of milk you will have:");
-//        int amountOfMilk = scanner.nextInt();
-//        System.out.println("Write how many grams of CoffeeBeams you will have:");
-//        int amountOfCoffeeBeams = scanner.nextInt();
-//        int coffeeForWater = amountOfWater / water;
-//        int coffeeForMilk = amountOfMilk / milk;
-//        int coffeeForCoffeeBeams = amountOfCoffeeBeams / coffeeBeams;
-//        int[] tab = {coffeeForWater, coffeeForMilk, coffeeForCoffeeBeams};
-//        int min = Arrays.stream(tab).min().getAsInt();
-//        System.out.println("Write how many cup of coffee you will need:");
-//        int cupOfCoffee = scanner.nextInt();
-//        int N = min - cupOfCoffee;
-//        if (min == cupOfCoffee) {
-//            System.out.println("Yes, i can make that amount of coffee");
-//        } else if (min > cupOfCoffee) {
-//            System.out.println("Yes, i can make that amount of coffee(and even" + " " + N + " " + "more than that)");
-//        } else {
-//            System.out.println("No,i make only" + " " + min + " " + "cups of coffee\n");
-//        }
+        System.out.println("Write how many ml of water you will have:");
+        int amountOfWater = scanner.nextInt();
+        System.out.println("Write many how ml of milk you will have:");
+        int amountOfMilk = scanner.nextInt();
+        System.out.println("Write how many grams of CoffeeBeams you will have:");
+        int amountOfCoffeeBeams = scanner.nextInt();
+        int coffeeForWater = amountOfWater / water;
+        int coffeeForMilk = amountOfMilk / milk;
+        int coffeeForCoffeeBeams = amountOfCoffeeBeams / coffeeBeams;
+        int[] tab = {coffeeForWater, coffeeForMilk, coffeeForCoffeeBeams};
+        int min = Arrays.stream(tab).min().getAsInt();
+        System.out.println("Write how many cup of coffee you will need:");
+        int cupOfCoffee = scanner.nextInt();
+        int N = min - cupOfCoffee;
+        if (min == cupOfCoffee) {
+            System.out.println("Yes, i can make that amount of coffee");
+        } else if (min > cupOfCoffee) {
+            System.out.println("Yes, i can make that amount of coffee(and even" + " " + N + " " + "more than that)");
+        } else {
+            System.out.println("No,i make only" + " " + min + " " + "cups of coffee\n");
+        }*/
 
-        //STAGE 4
+
+        //STAGE 4 //STAGE 5,6
 
         while (true) {
-            System.out.println(employee.showIngridients());
             System.out.println("Write action: buy, fill, take, remaining, exit:");
             String action = scanner.next();
             if (action.equals("buy")) {
                 employee.buyCoffee();
-                System.out.println(employee.showIngridients());
-                break;
             }
             if (action.equals("fill")) {
                 employee.incIngredients();
-                System.out.println(employee.showIngridients());
-                break;
             }
             else if (action.equals("take")) {
                 System.out.println("I gave you " + " " + employee.getMoney());
                 employee.takeMoney();
+            }
+            else  if (action.equals("remaining")){
                 System.out.println(employee.showIngridients());
+            }
+            else  if (action.equals("exit")){
                 break;
             }
         }
