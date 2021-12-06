a79e325d5046748b3b7b5bbb51a5d76fe9206c72
dsolomatinn <d.v.solomatin@student.khai.edu>
2021-12-06 20:40:46 +0200

    Stage2 calculator

--- src/nix/java/education/claculator/Main.java
+++ src/nix/java/education/claculator/Main.java
@@ -3,17 +3,10 @@ package nix.java.education.claculator;
 import java.util.Scanner;
 
 public class Main {
+    public static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Calculator calculator =new Calculator();
+        calculator.chooseAction();
 
-        //Stage 1
-
-        System.out.println("""
-                Loan principal: 1000
-                Month 1:repaid 250
-                Month 2:repaid 250
-                Month 3:repaid 500
-                The loan has been repaid
-                """);
     }
 }
