d74742967c37b141fc9c9379b68a7f9f80e316fd
dsolomatinn <d.v.solomatin@student.khai.edu>
2021-12-06 21:42:54 +0200

    Stage3 calculator

--- src/nix/java/education/calculator/Main.java
+++ src/nix/java/education/calculator/Main.java
@@ -1,12 +1,13 @@
-package nix.java.education.claculator;
+package nix.java.education.calculator;
 
 import java.util.Scanner;
 
 public class Main {
     public static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Calculator calculator =new Calculator();
-        calculator.chooseAction();
 
+        Calculator calculator =new Calculator();
+
+        calculator.chooseAction();
     }
 }
