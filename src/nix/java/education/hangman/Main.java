83aece2bfd2dd361a4a4cee241379cb6943cf2b9
dsolomatinn <d.v.solomatin@student.khai.edu>
2021-12-07 17:06:14 +0200

    STAGE #3

--- src/nix/java/education/hangman/Main.java
+++ src/nix/java/education/hangman/Main.java
@@ -1,11 +1,12 @@
 package nix.java.education.hangman;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println(
                 """
                 HANGMAN
@@ -13,8 +14,8 @@ public class Main {
 
         //STAGE 2
 
-        System.out.print("Guess the word: ");
-        String answerUser = scan.nextLine();
+        /* System.out.print("Guess the word: ");
+        String answerUser = scanner.nextLine();
         String word = "java";
         if (answerUser.equals(word)){
             System.out.println("You win");
@@ -22,7 +23,19 @@ public class Main {
         else{
             System.out.println("You lost!");
         }
-
+*/
+        //STAGE 3
+        Random random = new Random();
+        String[] words = {"java","python", "javascript", "kotlin"};
+        String randomWord = words[random.nextInt(words.length)];
+        System.out.print("Try to guess the word: ");
+        String answerUser = scanner.nextLine();
+        if (answerUser.equals(randomWord)){
+            System.out.println("You win");
+        }
+        else{
+            System.out.println("You lost!");
+        }
 
     }
 }
