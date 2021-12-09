76f4f5cc28463a578c960f3ba8e9b9644730587b
dsolomatinn <d.v.solomatin@student.khai.edu>
2021-12-09 19:48:16 +0200

    STAGE 1 tictac

--- src/nix/java/education/tictactoe/Main.java
+++ src/nix/java/education/tictactoe/Main.java
@@ -1,6 +1,14 @@
 package nix.java.education.tictactoe;
 import java.util.Scanner;
 public class Main {
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+        X X O
+        O X O
+        X O X
+        """);
+    }
+
 
 }
