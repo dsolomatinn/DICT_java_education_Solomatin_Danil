10b7d84561be664d51f24765f25f4f0fff098790
dsolomatinn <d.v.solomatin@student.khai.edu>
2021-12-19 21:21:17 +0200

    Stage 4 (Transport matrix)

--- src/nix/java/education/matrix/Matrix.java
+++ src/nix/java/education/matrix/Matrix.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 public class Matrix {
     Scanner scanner = new Scanner(System.in);
 
-    public void menu() {
-
+    public void menu(){
             while (true) {
-                System.out.println("""
+                System.out.print("""
                     Welcome to the matrix calculator
                     1. Sum of the matrix
                     2. Multiply matrix by constant
@@ -28,18 +27,37 @@ public class Matrix {
                     multiplyMatrix();
                 }
                 else  if (action.equals("4")){
-                    System.out.println("soon");
+                    menuTransport();
                 }
                 else  if (action.equals("exit")){
                     break;
                 }
             }
-
         }
 
+    public void menuTransport(){
+            System.out.print("""
+                1.Main diagonal
+                2.Side diagonal
+                3.Vertical line
+                4.Horizontal line
+                """);
+            String choice = scanner.next();
+            if (choice.equals("1")) {
+                transportMainDiagonal();
+            }
+            else if (choice.equals("2")) {
+                transportSideDiagonal();
+            }
+            else if (choice.equals("3")) {
+                transportVerticalLine();
+            }
+            else if (choice.equals("4")) {
+                transportHorizontalLine();
+            }
+        }
 
-
-    public void sumOfMatrix() {
+    public void sumOfMatrix(){
         System.out.print("Enter parametric n(lines): ");
         int n = scanner.nextInt();
         System.out.print("Enter parametric m (rows): ");
@@ -94,7 +112,7 @@ public class Matrix {
         }
     }
 
-    public void constantMatrix() {
+    public void constantMatrix(){
         System.out.print("Enter parametric n(lines): ");
         int n = scanner.nextInt();
         System.out.print("Enter parametric m (rows): ");
@@ -122,7 +140,7 @@ public class Matrix {
         }
     }
 
-    public void multiplyMatrix() {
+    public void multiplyMatrix(){
         int[] sizeFirstMatrix = new int[2];
         System.out.print("Enter size of first matrix : ");
         for (int i = 0; i < 2; i++) {
@@ -186,4 +204,144 @@ public class Matrix {
             System.out.println("ERROR");
         }
     }
+
+    public void transportMainDiagonal(){
+        int[] sizeMatrix = new int[2];
+        System.out.print("Enter size of first matrix : ");
+        for (int i = 0; i < 2; i++) {
+            sizeMatrix[i] = scanner.nextInt();
+        }
+        double[][] myMatrix = new double[sizeMatrix[0]][sizeMatrix[1]];
+        System.out.println("Enter the values of first matrix : ");
+        for (int i = 0; i < sizeMatrix[0]; i++) {
+            for (int j = 0; j < sizeMatrix[1]; j++) {
+                myMatrix[i][j] = scanner.nextDouble();
+            }
+        }
+
+        System.out.println("First Matrix: ");
+        for (int i = 0; i < sizeMatrix[0]; i++) {
+            for (int j = 0; j < sizeMatrix[1]; j++) {
+                System.out.print(myMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        double[][] transposeMatrix = new double[sizeMatrix[1]][sizeMatrix[0]];
+
+        System.out.println("Transpose matrix: ");
+        for (int n = 0; n < sizeMatrix[1]; n++) {
+            for (int m = 0; m < sizeMatrix[0]; m++) {
+                transposeMatrix[n][m] = myMatrix[m][n];
+                System.out.print(transposeMatrix[n][m] + " ");
+            }System.out.println();
+        }
+    }
+
+    public void transportSideDiagonal(){
+        int[] sizeMatrix = new int[2];
+        System.out.print("Enter size of first matrix : ");
+        for (int i = 0; i < 2; i++) {
+            sizeMatrix[i] = scanner.nextInt();
+        }
+        double[][] myMatrix = new double[sizeMatrix[0]][sizeMatrix[1]];
+        System.out.println("Enter the values of first matrix : ");
+        for (int i = 0; i < sizeMatrix[0]; i++) {
+            for (int j = 0; j < sizeMatrix[1]; j++) {
+                myMatrix[i][j] = scanner.nextDouble();
+            }
+        }
+
+        System.out.println("First Matrix: ");
+        for (int i = 0; i < sizeMatrix[0]; i++) {
+            for (int j = 0; j < sizeMatrix[1]; j++) {
+                System.out.print(myMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        double[][] transposeMatrix = new double[sizeMatrix[1]][sizeMatrix[0]];
+
+        System.out.println("Transpose matrix: ");
+        for (int n = 0; n < sizeMatrix[0]; n++) {
+            for (int m = 0; m < sizeMatrix[1]; m++) {
+                transposeMatrix[m][n] = myMatrix[sizeMatrix[0] - 1 - n][sizeMatrix[1] - 1 - m];
+                System.out.print(transposeMatrix[m][n] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void transportVerticalLine(){
+        int[] sizeMatrix = new int[2];
+        System.out.print("Enter size of first matrix : ");
+        for (int i = 0; i < 2; i++) {
+            sizeMatrix[i] = scanner.nextInt();
+        }
+        double[][] myMatrix = new double[sizeMatrix[0]][sizeMatrix[1]];
+        System.out.println("Enter the values of first matrix : ");
+        for (int i = 0; i < sizeMatrix[0]; i++) {
+            for (int j = 0; j < sizeMatrix[1]; j++) {
+                myMatrix[i][j] = scanner.nextDouble();
+            }
+        }
+
+        System.out.println("First Matrix: ");
+        for (int i = 0; i < sizeMatrix[0]; i++) {
+            for (int j = 0; j < sizeMatrix[1]; j++) {
+                System.out.print(myMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        double[][] transposeMatrix = new double[sizeMatrix[1]][sizeMatrix[0]];
+
+        System.out.println("Transpose matrix: ");
+        for (int n = 0; n < sizeMatrix[0]; n++) {
+            for (int m = 0; m < sizeMatrix[1]; m++) {
+                transposeMatrix[n][m] = myMatrix[n][sizeMatrix[1] - 1 - m];
+                System.out.print(transposeMatrix[n][m] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void transportHorizontalLine(){
+        int[] sizeMatrix = new int[2];
+        System.out.print("Enter size of first matrix : ");
+        for (int i = 0; i < 2; i++) {
+            sizeMatrix[i] = scanner.nextInt();
+        }
+        double[][] myMatrix = new double[sizeMatrix[0]][sizeMatrix[1]];
+        System.out.println("Enter the values of first matrix : ");
+        for (int i = 0; i < sizeMatrix[0]; i++) {
+            for (int j = 0; j < sizeMatrix[1]; j++) {
+                myMatrix[i][j] = scanner.nextDouble();
+            }
+        }
+
+        System.out.println("First Matrix: ");
+        for (int i = 0; i < sizeMatrix[0]; i++) {
+            for (int j = 0; j < sizeMatrix[1]; j++) {
+                System.out.print(myMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        double[][] transposeMatrix = new double[sizeMatrix[1]][sizeMatrix[0]];
+
+        System.out.println("Transpose matrix: ");
+        for (int n = 0; n < sizeMatrix[0]; n++) {
+            for (int m = 0; m < sizeMatrix[1]; m++) {
+                transposeMatrix[n][m] = myMatrix[sizeMatrix[0] - 1 - n][m];
+                System.out.print(transposeMatrix[n][m] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+
 }
